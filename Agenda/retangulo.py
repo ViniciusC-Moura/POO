b0d90b0767c6fcb5132fbda35a41b7429f3e8776
ViniciusC-Moura <vinicius.cavalcanti@escolar.ifrn.edu.br>
2025-09-11 13:25:39 +0000

    streamlit 1

---
+++ Agenda/retangulo.py
@@ -0,0 +1,13 @@
+class Retangulo():
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calc_area(self):
+        return self.base * self.altura
+    
+    def calc_diagonal(self):
+        return (self.base ** 2 + self.altura ** 2) ** 0.5
+
+    def __str__(self):
+        return f"Retângulo | Base: {self.base} Altura: {self.altura} Área: {self.calc_area()} Diagonal: {self.calc_diagonal}"
