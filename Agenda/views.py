c6c07dc76a70f6d73804bcf927e41a570b2fcfc2
ViniciusC-Moura <vinicius.cavalcanti@escolar.ifrn.edu.br>
2025-09-30 14:50:55 +0000

    login de cliente implementado

--- Agenda/views.py
+++ Agenda/views.py
@@ -12,19 +12,30 @@ from models.profissionalDAO import ProfissionalDAO
 
 class View:
 
+    def cliente_criar_admin():
+        for c in View.cliente_listar():
+            if c.get_email() == "admin": return
+        View.cliente_inserir("admin", "admin", "fone", "1234")
+
+    def cliente_autenticar(email, senha):
+        for c in View.cliente_listar():
+            if c.get_email() == email and c.get_senha() == senha:
+                return {"id": c.get_id(), "nome": c.get_nome()}
+        return None
+
     def cliente_listar():
         return ClienteDAO.listar()
     def cliente_listar_id(id):
         c = ClienteDAO.listar_id(id)
         return c
-    def cliente_inserir(nome, email, fone):
-        cliente = Cliente(0, nome, email, fone)
+    def cliente_inserir(nome, email, fone, senha):
+        cliente = Cliente(0, nome, email, fone, senha)
         ClienteDAO.inserir(cliente)
-    def cliente_atualizar(id, nome, email, fone):
+    def cliente_atualizar(id, nome, email, fone, senha):
         cliente = Cliente(id, nome, email, fone)
         ClienteDAO.atualizar(cliente)
     def cliente_excluir(id):
-        cliente = Cliente(id, "", "", "")
+        cliente = Cliente(id, "", "", "", "")
         ClienteDAO.excluir(cliente)
 
     def servico_listar():
